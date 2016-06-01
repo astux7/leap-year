139b9181bc05a4892a52c36d5b8ff000d814ac3a
Asta Bevainyte <asta.bevainyte@sky.uk>
2016-06-01 19:42:16 +0100

    refactor step 1

--- public/src/Year.js
+++ public/src/Year.js
@@ -1,11 +1,7 @@
 function isLeapYear(yr) {
+
   switch (true) {
     case (yr % 4 !== 0):
-      return "common";
-      break;
-    case (yr % 100 !== 0):
-      return "leap";
-      break;
     case (yr % 400 !== 0):
       return "common";
       break;
